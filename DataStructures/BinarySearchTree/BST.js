e2b1f056341ec8d872917da1e0da5934c29804bc
JaiRaga <raga.j.santhosh@gmail.com>
2019-06-05 18:55:10 +0530

    DFS post and in order implementation

--- DataStructures/BinarySearchTree/BST.js
+++ DataStructures/BinarySearchTree/BST.js
@@ -80,6 +80,28 @@ class BST {
         traverse(this.root);
         return data;
     } 
+
+    DFSPostOrder() {
+        let data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DFSInOrder() {
+        let data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            data.push(node.value);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 let b = new BST();
@@ -94,3 +116,5 @@ console.log(b);
 
 console.log(b.BFS());
 console.log(b.DFSPreOrder());
+console.log(b.DFSPostOrder());
+console.log(b.DFSInOrder());
